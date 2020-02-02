5a3778693866efbc0ea429c5b9fcbf5a246936dc
Victor Pajeu <pajeuvictor@gmail.com>
2020-02-02 11:39:16 -0300

    ADD detalhes do objeto da API

---
+++ app/src/main/java/br/com/victorpajeu/books/ui/bookDetailActivity.kt
@@ -0,0 +1,12 @@
+package br.com.victorpajeu.books
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class bookDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_book_detail)
+    }
+}
